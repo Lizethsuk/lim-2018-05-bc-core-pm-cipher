362f35824dd010b33e605990c2caf26d3763018f
Lizethsuk <julia.sucasacai@gmail.com>
2018-05-23 17:56:35 -0500

    Subiendo cambios

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-const mensajeCifrado=document.getElementById('mensaje')
-const buttonCifrar=buttonCifrar.addEventListener('click',mensajeCifrado)=>{
-  document.getElementById('resultado')
+const texto1= document.getElementById('mensaje');
+const botonCifrar=document.getElementById('cifrar');
+botonCifrar.addEventListener("click", traducir);
+function traducir() {
+  const producto=texto1.value;
+  mensaje0.value= producto;
 }
+
+const botonDescifrar=document.getElementById('descifrar');
+botonDescifrar.addEventListener('click', traducir2);
+function traducir2() {
+  const producto2=texto1.value;
+  mensaje0.value=producto2
+}
+
+const mensaje0=document.getElementById('resultado');
