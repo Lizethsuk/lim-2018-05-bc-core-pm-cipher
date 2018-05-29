814580cd8091d9882b3cd7cae517b257c0921f44
Lizethsuk <julia.sucasacai@gmail.com>
2018-05-29 13:58:23 -0500

    codigo hecho

--- src/index.js
+++ src/index.js
@@ -1,42 +1,53 @@
 let texto=document.getElementById("mensaje");
+let offSet=document.getElementById('numero');
 let respuesta=document.getElementById('resultado');
 const botonCifrar=document.getElementById('cifrar');
-botonCifrar.addEventListener("click", codificar)
+botonCifrar.addEventListener("click", encode)
 function encode() {
   let raro="";
   let palabra=texto.value;
+  let n=Math.abs(parseInt(offSet.value));
   for(let i=0;i<palabra.length;i++){
   let ascci=palabra.charCodeAt(i);
   if (ascci>=65 && ascci<=90) {
-    let grande= String.fromCharCode((ascci-65+6)%26+65);
-    raro+= grande;
+    let code= String.fromCharCode((ascci-65+n)%26+65);
+    raro+= code;
   } else if (ascci>=97 && ascci<=122){
-    let chica= String.fromCharCode((ascci-97+6)%26+97);
-    raro+= chica;
+    let code= String.fromCharCode((ascci-97+n)%26+97);
+    raro+= code;
   } else {
-    let otro= String.fromCharCode(ascci);
-    raro+= otro;
+    let code= String.fromCharCode(ascci);
+    raro+= code;
     }
   }
 respuesta.value= raro;
 }
 const botonDescifrar=document.getElementById('descifrar');
-botonDescifrar.addEventListener('click', decodificar);
+botonDescifrar.addEventListener('click', decode);
 function decode() {
-let rara="";
+let raro="";
 let palabra=texto.value;
+let n=Math.abs(parseInt(offSet.value));
 for(let i=0;i<palabra.length;i++){
 let ascci=palabra.charCodeAt(i);
 if (ascci>=65 && ascci<=90) {
-  let grande= String.fromCharCode((ascci-65+20)%26+65);
-  rara+= grande;
+  if ((ascci-n-65)%26==0||(ascci-n-65)%26>0) {
+    let code= String.fromCharCode((ascci-65-n)%26+65);
+  raro+= code;
+} else {let code= String.fromCharCode((ascci-65-n)%26+91);
+    raro+= code;
+  }
 } else if (ascci>=97 && ascci<=122){
-  let chica= String.fromCharCode((ascci-97+20)%26+97);
-  rara+= chica;
+  if ((ascci-n-97)%26==0||(ascci-n-97)%26>0) {
+    let code= String.fromCharCode((ascci-97-n)%26 +97);
+  raro+= code;
+} else {let code= String.fromCharCode((ascci-97-n)%26+123);
+    raro+= code;
+  }
 } else {
-  let otro= String.fromCharCode(ascci);
-  rara+= otro;
+  let code= String.fromCharCode(ascci);
+  raro+= code;
   }
 }
-respuesta.value= rara;
+respuesta.value= raro;
 }
