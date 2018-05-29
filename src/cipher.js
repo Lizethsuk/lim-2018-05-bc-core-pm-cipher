e76f55b7e79d5686062fc899a07ed2f29713e331
Lizethsuk <julia.sucasacai@gmail.com>
2018-05-29 16:26:10 -0500

    subiendo cipher, js, css

--- src/cipher.js
+++ src/cipher.js
@@ -1,44 +1,54 @@
 window.cipher = {
-  encode: function () {
-  let raro="";
-  let palabra=texto.value;
-  for(let i=0;i<palabra.length;i++){
-  let ascci=palabra.charCodeAt(i);
-  if (ascci>=65 && ascci<=90) {
-    let grande= String.fromCharCode((ascci-65+33)%26+65);
-    raro+= grande;
-  } else if (ascci>=97 && ascci<=122){
-    let chica= String.fromCharCode((ascci-97+33)%26+97);
-    raro+= chica;
-  } else {
-    let otro= String.fromCharCode(ascci);
-    raro+= otro;
-    }
-  }
-respuesta.value= raro;
-}
+  encode: function() {
+      let raro="";
+      let palabra=texto.value;
+      let n=Math.abs(parseInt(offSet.value));
+      for(let i=0;i<palabra.length;i++){
+      let ascci=palabra.charCodeAt(i);
+      if (ascci>=65 && ascci<=90) {
+        let code= String.fromCharCode((ascci-65+n)%26+65);
+        raro+= code;
+      } else if (ascci>=97 && ascci<=122){
+        let code= String.fromCharCode((ascci-97+n)%26+97);
+        raro+= code;
+      } else {
+        let code= String.fromCharCode(ascci);
+        raro+= code;
+        }
+      }
+    respuesta.value= raro;
+  },
 
-decode: function () {
-let rara="";
-let palabra=texto.value;
-for(let i=0;i<palabra.length;i++){
-let ascci=palabra.charCodeAt(i);
-if (ascci>=65 && ascci<=90) {
-  let grande= String.fromCharCode((ascci-65-33)%26+65);
-  rara+= grande;
-} else if (ascci>=97 && ascci<=122){
-  let chica= String.fromCharCode((ascci-97-33)%26+97);
-  rara+= chica;
-} else {
-  let otro= String.fromCharCode(ascci);
-  rara+= otro;
-  }
-}
-respuesta.value= rara;
-}
-};
-createCipherWithOffset: function() {
+decode: function() {
+    let raro="";
+    let palabra=texto.value;
+    let n=Math.abs(parseInt(offSet.value));
+    for(let i=0;i<palabra.length;i++){
+    let ascci=palabra.charCodeAt(i);
+    if (ascci>=65 && ascci<=90) {
+      if ((ascci-n-65)%26==0||(ascci-n-65)%26>0) {
+        let code= String.fromCharCode((ascci-65-n)%26+65);
+      raro+= code;
+    } else {let code= String.fromCharCode((ascci-65-n)%26+91);
+        raro+= code;
+      }
+    } else if (ascci>=97 && ascci<=122){
+      if ((ascci-n-97)%26==0||(ascci-n-97)%26>0) {
+        let code= String.fromCharCode((ascci-97-n)%26 +97);
+      raro+= code;
+    } else {let code= String.fromCharCode((ascci-97-n)%26+123);
+        raro+= code;
+      }
+    } else {
+      let code= String.fromCharCode(ascci);
+      raro+= code;
+      }
+    }
+    respuesta.value= raro;
+  },
+  createCipherWithOffset: function(){
   const palabra="abcdefghijklmnopqrstuvwxyz";
-  const metodo1=cipher.encode(palabra);
-  const metodo2=cipher.decode(palabra);
+  const metod=cipher.encode(palabra);
+  const metod2=cipher.decode(palabra);
+}
 }
--- src/index.js
+++ src/index.js
@@ -2,52 +2,6 @@ let texto=document.getElementById("mensaje");
 let offSet=document.getElementById('numero');
 let respuesta=document.getElementById('resultado');
 const botonCifrar=document.getElementById('cifrar');
-botonCifrar.addEventListener("click", encode)
-function encode() {
-  let raro="";
-  let palabra=texto.value;
-  let n=Math.abs(parseInt(offSet.value));
-  for(let i=0;i<palabra.length;i++){
-  let ascci=palabra.charCodeAt(i);
-  if (ascci>=65 && ascci<=90) {
-    let code= String.fromCharCode((ascci-65+n)%26+65);
-    raro+= code;
-  } else if (ascci>=97 && ascci<=122){
-    let code= String.fromCharCode((ascci-97+n)%26+97);
-    raro+= code;
-  } else {
-    let code= String.fromCharCode(ascci);
-    raro+= code;
-    }
-  }
-respuesta.value= raro;
-}
+botonCifrar.addEventListener("click", cipher.encode);
 const botonDescifrar=document.getElementById('descifrar');
-botonDescifrar.addEventListener('click', decode);
-function decode() {
-let raro="";
-let palabra=texto.value;
-let n=Math.abs(parseInt(offSet.value));
-for(let i=0;i<palabra.length;i++){
-let ascci=palabra.charCodeAt(i);
-if (ascci>=65 && ascci<=90) {
-  if ((ascci-n-65)%26==0||(ascci-n-65)%26>0) {
-    let code= String.fromCharCode((ascci-65-n)%26+65);
-  raro+= code;
-} else {let code= String.fromCharCode((ascci-65-n)%26+91);
-    raro+= code;
-  }
-} else if (ascci>=97 && ascci<=122){
-  if ((ascci-n-97)%26==0||(ascci-n-97)%26>0) {
-    let code= String.fromCharCode((ascci-97-n)%26 +97);
-  raro+= code;
-} else {let code= String.fromCharCode((ascci-97-n)%26+123);
-    raro+= code;
-  }
-} else {
-  let code= String.fromCharCode(ascci);
-  raro+= code;
-  }
-}
-respuesta.value= raro;
-}
+botonDescifrar.addEventListener('click', cipher.decode);
