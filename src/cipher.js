d45274b23821aacbf8aec55e67d419cda244350d
Lizethsuk <julia.sucasacai@gmail.com>
2018-05-31 16:21:03 -0500

    ya pasa test en encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,21 @@
 window.cipher = {
-  encode: function(offset,string) {
-      let n=parseInt(offset);
-      let raro="";
-      for(let i=0;i<string.length;i++){
-      let ascci=string.charCodeAt(i);
-      if (ascci>=65 && ascci<=90) {
-        let code= String.fromCharCode((ascci-65+n)%26+65);
-        raro+= code;
+  encode: (offset,string) => {
+      let output="";
+      for(let i = 0; i < string.length; i++){
+      let ascci = string.charCodeAt(i);
+      if (ascci >= 65 && ascci <= 90) {
+        let code= String.fromCharCode((ascci-65+parseInt(offset))%26+65);
+        output += code;
       } else if (ascci>=97 && ascci<=122){
-        let code= String.fromCharCode((ascci-97+n)%26+97);
-        raro+= code;
+        let code = String.fromCharCode((ascci-97+parseInt(offset))%26+97);
+        output += code;
       } else {
         let code= String.fromCharCode(ascci);
-        raro+= code;
+        output += code;
         }
       }
-        return raro;
+        return output;
   },
-
 decode: function(offset,string) {
     let n=parseInt(offset);
     let raro="";
@@ -45,3 +43,6 @@ decode: function(offset,string) {
     return raro;
   },
 }
+
+
+//console.log(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'));
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,16 +1,17 @@
-describe('cipher', () => {
+describe('El objeto cipher', () => {
 
-  it('debería ser un objeto', () => {
+  it('cipher debería ser un objeto', () => {
     assert.equal(typeof cipher, 'object');
   });
 
-  describe('cipher.encode', () => {
+  describe('ahora voy a probar la propiedad cipher.encode', () => {
 
-    it('debería ser una función', () => {
+    it(' cipher.encode debería ser una función', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
-
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+      it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',()=>{
+       assert.equal(cipher.encode("33",'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),'HIJKLMNOPQRSTUVWXYZABCDEFG');
+     });
   });
 
   describe('cipher.decode', () => {
@@ -19,7 +20,10 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',()=>{
+      assert.equal(cipher.decode("33",'HIJKLMNOPQRSTUVWXYZABCDEFG'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
   });
 
   describe('cipher.createCipherWithOffset', () => {
